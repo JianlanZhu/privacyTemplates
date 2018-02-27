2fafcad5021fbab0e6be21056eba369962d10109
Fabio Bove <bove@in.tum.de>
2018-02-26 22:58:46 -0500

    Extended example

--- src/main/java/edu/cmu/PrivacyTemplatesApplication.java
+++ src/main/java/edu/cmu/PrivacyTemplatesApplication.java
@@ -1,12 +1,32 @@
 package edu.cmu;
 
+import edu.cmu.api.EmployeesResource;
+import edu.cmu.db.dao.EmployeeDAO;
+import edu.cmu.db.entities.Employee;
 import edu.cmu.health.TemplateHealthCheck;
 import edu.cmu.resources.HelloWorldResource;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class PrivacyTemplatesApplication extends Application<PrivacyTemplatesConfiguration> {
+
+    /**
+     * Hibernate bundle.
+     */
+    private final HibernateBundle<PrivacyTemplatesConfiguration> hibernateBundle
+            = new HibernateBundle<PrivacyTemplatesConfiguration>(
+            Employee.class
+    ) {
+        @Override
+        public DataSourceFactory getDataSourceFactory(
+                PrivacyTemplatesConfiguration configuration
+        ) {
+            return configuration.getDataSourceFactory();
+        }
+    };
 
     public static void main(final String[] args) throws Exception {
         new PrivacyTemplatesApplication().run(args);
@@ -19,12 +39,15 @@ public class PrivacyTemplatesApplication extends Application<PrivacyTemplatesCon
 
     @Override
     public void initialize(final Bootstrap<PrivacyTemplatesConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(hibernateBundle);
     }
 
     @Override
     public void run(PrivacyTemplatesConfiguration configuration,
                     Environment environment) {
+
+        environment.jersey().register(new EmployeesResource(new EmployeeDAO(hibernateBundle.getSessionFactory())));
+
         environment.jersey().register(
                 new HelloWorldResource(
                         configuration.getTemplate(),
--- src/main/java/edu/cmu/PrivacyTemplatesConfiguration.java
+++ src/main/java/edu/cmu/PrivacyTemplatesConfiguration.java
@@ -3,9 +3,24 @@ package edu.cmu;
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
+import io.dropwizard.db.DataSourceFactory;
+
 public class PrivacyTemplatesConfiguration extends Configuration {
+
+    /**
+     * Factories are used by Dropwizard to group together related configuration
+     * parameters such as database connection driver, URI, password etc.
+     */
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory
+            = new DataSourceFactory();
+
+
     @NotEmpty
     private String template;
 
@@ -30,5 +45,14 @@ public class PrivacyTemplatesConfiguration extends Configuration {
     @JsonProperty
     public void setDefaultName(String name) {
         this.defaultName = name;
+    }
+
+    /**
+     * @return An instance of database factory deserialized from the
+     * configuration file passed as a command-line argument to the application.
+     */
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
     }
 }
