d2833bdefe5efd00214934c787f7243d06b2828e
Fabio Bove <bove@in.tum.de>
2018-03-06 15:00:04 -0500

    Added simple endpoint for generating request

--- src/main/java/edu/cmu/resources/RequestResource.java
+++ src/main/java/edu/cmu/resources/RequestResource.java
@@ -3,21 +3,22 @@ package edu.cmu.resources;
 import com.codahale.metrics.annotation.Timed;
 import edu.cmu.resources.interaction.SayingOutput;
 import edu.cmu.resources.interaction.GenerateRequestInput;
+import org.assertj.core.util.Strings;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/request")
-@Produces(MediaType.APPLICATION_JSON)
 public class RequestResource {
 
     @POST
     @Path("/generate")
     @Consumes(MediaType.APPLICATION_JSON)
     @Timed
-    public SayingOutput generateRequest(GenerateRequestInput generateRequestInput) {
-        final String value = String.format("Sent in : %s", generateRequestInput.getContent());
-        return new SayingOutput(1, value);
+    public void generateRequest(GenerateRequestInput generateRequestInput) {
+        if(generateRequestInput.getCaseNumber() <= 0 || Strings.isNullOrEmpty(generateRequestInput.getSuspectUserName())){
+            throw new BadRequestException();
+        }
     }
 }
 
