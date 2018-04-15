7e74dcae9f34fdc10e043ae4cb2afd4097a3ce52
Fabio Bove <bove@in.tum.de>
2018-04-14 22:01:43 -0400

    Simple data upload form

--- src/main/java/edu/cmu/resources/SocialMediaResource.java
+++ src/main/java/edu/cmu/resources/SocialMediaResource.java
@@ -4,8 +4,8 @@ import com.codahale.metrics.annotation.Timed;
 import edu.cmu.db.dao.RequestDAO;
 import edu.cmu.db.entities.Request;
 import edu.cmu.db.entities.User;
-import edu.cmu.resources.interaction.DataUploadInput;
 import edu.cmu.resources.views.UnansweredRequestsView;
+import edu.cmu.resources.views.UploadDataFormView;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 import io.dropwizard.views.View;
@@ -15,7 +15,11 @@ import org.glassfish.jersey.media.multipart.FormDataParam;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
+import java.util.zip.ZipInputStream;
 
 @Path("/socialMedia")
 @RolesAllowed("SOCIAL_MEDIA_EMPLOYEE")
@@ -23,44 +27,68 @@ public class SocialMediaResource {
 
     private RequestDAO requestDAO;
 
-    public SocialMediaResource(RequestDAO requestDAO){
+    public SocialMediaResource(RequestDAO requestDAO) {
         this.requestDAO = requestDAO;
     }
 
     @GET
     @Path("/unansweredRequests")
-    public View listAllUnansweredRequests(){
+    public View listAllUnansweredRequests() {
         return new UnansweredRequestsView();
     }
 
     @POST
     @Path("DataUploadForm")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed("SOCIAL_MEDIA_EMPLOYEE")
     @UnitOfWork
     @Timed
     public void uploadData(@Auth User user,
                            @FormDataParam("data") final FormDataBodyPart fileField,
-                           @FormDataParam("information") FormDataBodyPart generateRequestInput) {
+                           @FormDataParam("caseID") FormDataBodyPart requestId,
+                           @FormDataParam("comment") FormDataBodyPart comment) {
 
-        generateRequestInput.setMediaType(MediaType.APPLICATION_JSON_TYPE);
-        DataUploadInput parsedInput = generateRequestInput.getValueAs(DataUploadInput.class);
+        if (fileField != null) {
+            InputStream warrantFileInputStream = new BufferedInputStream(fileField.getValueAs(InputStream.class));
+            try {
+                // getNextEntry returns null if the InputStream is not a zip file
+                if (new ZipInputStream(warrantFileInputStream).getNextEntry() == null) {
+                    throw new BadRequestException("Uploaded file was not a zip file!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-        Optional<Request> requestOptional = requestDAO.findById(parsedInput.getRequestId());
-        if(requestOptional.isPresent()){
+        requestId.setMediaType(MediaType.TEXT_PLAIN_TYPE);
+        int requestIdNumber = Integer.parseInt(requestId.getValue());
+        comment.setMediaType(MediaType.TEXT_PLAIN_TYPE);
+
+
+        Optional<Request> requestOptional = requestDAO.findById(requestIdNumber);
+        if (requestOptional.isPresent()) {
             Request request = requestOptional.get();
-            if(request.getStatus() == null || request.getStatus().equals("FILED")){
+            if (request.getStatus() == null || request.getStatus().equals("FILED")) {
                 boolean success = requestDAO.updateStatus(request.getRequestID(), "ANSWERED");
-                if(!success){ throw new NotFoundException(); }
+                if (!success) {
+                    throw new NotFoundException();
+                }
+                System.out.println("Success: " + comment.getValue());
                 // TODO handle uploaded data
-            } else{
+            } else {
                 throw new BadRequestException("Request has already been answered or rejected.");
             }
-        } else{
+        } else {
             throw new BadRequestException("Request ID invalid");
         }
 
+    }
+
+    @GET
+    @Path("DataUploadForm")
+    @UnitOfWork
+    @Timed
+    public UploadDataFormView getUploadForm() {
+        return new UploadDataFormView();
     }
 
 }
