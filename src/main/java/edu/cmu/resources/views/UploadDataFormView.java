5260d8ffc01544ad6310ca23b5533c542801ea0f
Fabio Bove <bove@in.tum.de>
2018-04-17 21:31:53 -0400

    Use request id automatically on SM uplaod

--- src/main/java/edu/cmu/resources/views/UploadDataFormView.java
+++ src/main/java/edu/cmu/resources/views/UploadDataFormView.java
@@ -3,7 +3,14 @@ package edu.cmu.resources.views;
 import io.dropwizard.views.View;
 
 public class UploadDataFormView extends View {
-    public UploadDataFormView(){
+    private int requestId;
+
+    public UploadDataFormView(int requestId) {
         super("uploadDataForm.mustache");
+        this.requestId = requestId;
+    }
+
+    public int getRequestId() {
+        return requestId;
     }
 }
