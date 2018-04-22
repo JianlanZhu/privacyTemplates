237675a90b6b98b4593f5775785b55e909eb877c
Fabio Bove <bove@in.tum.de>
2018-04-22 14:52:47 -0400

    tests for the auth package

---
+++ src/test/java/edu/cmu/auth/TokenAuthenticatorTest.java
@@ -0,0 +1,73 @@
+package edu.cmu.auth;
+
+import edu.cmu.db.dao.TokenDAO;
+import edu.cmu.db.entities.Token;
+import edu.cmu.db.entities.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class TokenAuthenticatorTest {
+
+    private TokenAuthenticator tokenAuthenticator;
+    private TokenDAO tokenDAO;
+
+    @Before
+    public void setup() {
+        tokenDAO = mock(TokenDAO.class);
+        tokenAuthenticator = new TokenAuthenticator(tokenDAO);
+    }
+
+    @Test
+    public void authenticateValidToken() {
+        final String validTokenString = "validToken";
+        final Instant validUntil = Instant.now().plus(Duration.ofMinutes(10));
+        final User userWithValidToken = new User();
+        userWithValidToken.setUserName("validUser");
+        final Token validToken = new Token(userWithValidToken, validTokenString, validUntil);
+
+        when(tokenDAO.findUserByToken(validTokenString)).thenReturn(Optional.of(validToken));
+
+        Optional<User> userOptional = tokenAuthenticator.authenticate(validTokenString);
+
+        assertThat(userOptional).isPresent();
+
+        User user = userOptional.get();
+
+        assertThat(user.getUserName()).isEqualToIgnoringCase("validUser");
+
+    }
+
+    @Test
+    public void rejectExpiredToken() {
+        final String expiredTokenString = "expiredToken";
+        final Instant validUntil = Instant.now().minus(Duration.ofMinutes(10));
+        final User userWithExpiredToken = mock(User.class);
+        final Token expiredToken = new Token(userWithExpiredToken, expiredTokenString, validUntil);
+
+        when(tokenDAO.findUserByToken(expiredTokenString)).thenReturn(Optional.of(expiredToken));
+
+        Optional<User> userOptional = tokenAuthenticator.authenticate(expiredTokenString);
+
+        assertThat(userOptional).isEmpty();
+    }
+
+    @Test
+    public void rejectUnknownToken() {
+        final String unknownTokenString = "unknownToken";
+
+        when(tokenDAO.findUserByToken(unknownTokenString)).thenReturn(Optional.empty());
+
+        Optional<User> userOptional = tokenAuthenticator.authenticate(unknownTokenString);
+
+        assertThat(userOptional).isEmpty();
+    }
+}
