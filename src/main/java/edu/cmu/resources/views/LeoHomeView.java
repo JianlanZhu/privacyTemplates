65ec9490bfd3cf357d93e6efd58d904901dee159
Fabio Bove <bove@in.tum.de>
2018-04-24 20:24:58 -0400

    sort requests by status

--- src/main/java/edu/cmu/resources/views/LeoHomeView.java
+++ src/main/java/edu/cmu/resources/views/LeoHomeView.java
@@ -6,14 +6,32 @@ import io.dropwizard.views.View;
 import java.util.List;
 
 public class LeoHomeView extends View {
-    private List<Request> requests;
+    private List<Request> rejectedRequests;
+    private List<Request> answeredRequests;
+    private List<Request> pendingRequests;
+    private List<Request> closedRequests;
 
-    public LeoHomeView(List<Request> requests) {
+    public LeoHomeView(List<Request> rejectedRequests, List<Request> answeredRequests, List<Request> pendingRequests, List<Request> closedRequests) {
         super("leohome.mustache");
-        this.requests = requests;
+        this.rejectedRequests = rejectedRequests;
+        this.answeredRequests = answeredRequests;
+        this.pendingRequests = pendingRequests;
+        this.closedRequests = closedRequests;
     }
 
-    public List<Request> getRequests() {
-        return requests;
+    public List<Request> getRejectedRequests() {
+        return rejectedRequests;
+    }
+
+    public List<Request> getAnsweredRequests() {
+        return answeredRequests;
+    }
+
+    public List<Request> getPendingRequests() {
+        return pendingRequests;
+    }
+
+    public List<Request> getClosedRequests() {
+        return closedRequests;
     }
 }
