20909c77f6dcbbee5da5cfbe9cb56aeb1cf9ae0e
Fabio Bove <bove@in.tum.de>
2018-05-01 16:10:49 -0400

    Adjust sample data for presentation

--- src/main/resources/db/migration/V100__insert_sample_data.sql
+++ src/main/resources/db/migration/V100__insert_sample_data.sql
@@ -1,23 +1,31 @@
 INSERT INTO retentiontype (retentionID, retentionDays) VALUES (1, 180);
 INSERT INTO retentiontype (retentionID, retentionDays) VALUES (2, 360);
 
-INSERT INTO User (userType, userName, password, accountEnabled, salt) VALUES
-  ("LAW_ENFORCEMENT_OFFICER", "SaSm", "32C6636677E3634D64DD5B73EB4D3AB78FD8AB4D19C2497176A2D6D752AF1C98", 1,
+INSERT INTO User (userId, userType, userName, password, accountEnabled, salt) VALUES
+  (1, "LAW_ENFORCEMENT_OFFICER", "SamSmalltown", "32C6636677E3634D64DD5B73EB4D3AB78FD8AB4D19C2497176A2D6D752AF1C98", 1,
    "SamsSalt");
-INSERT INTO User (userType, userName, password, accountEnabled, salt) VALUES
-  ("LAW_ENFORCEMENT_OFFICER", "AlAn", "E3D2DFE19FD55BF5B7DC29C24D07CA295A5C6545A657C7D769358387E6DED845", 1,
+INSERT INTO User (userId, userType, userName, password, accountEnabled, salt) VALUES
+  (2, "LAW_ENFORCEMENT_OFFICER", "AlanAnalyst", "E3D2DFE19FD55BF5B7DC29C24D07CA295A5C6545A657C7D769358387E6DED845", 1,
    "AlansSalt");
-INSERT INTO User (userType, userName, password, accountEnabled, salt) VALUES
-  ("SOCIAL_MEDIA_EMPLOYEE", "HaHa", "43E12EE3BAF37F13892364B0D4F9623124F2BB937D997944FFFEABCF0D902E86", 1, "HalysSalt");
+INSERT INTO User (userId, userType, userName, password, accountEnabled, salt) VALUES
+  (3, "SOCIAL_MEDIA_EMPLOYEE", "HaylieHipster", "43E12EE3BAF37F13892364B0D4F9623124F2BB937D997944FFFEABCF0D902E86", 1,
+   "HalysSalt");
 
 INSERT INTO result (resultID, SMEUserID, retentionID) VALUES (1, 3, 1);
 
 INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status, requestedDataStartDate, requestedDataEndDate)
-VALUES (1, "Suspect 1", "MISDEMEANEOR", 2, "PENDING", "2013-10-1", "2017-12-20");
-INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status, socialMediaComment, resultID)
-VALUES (112, "Suspect 2", "FELONY", 2, "ANSWERED", "Attached, see the requested data.", 1);
+VALUES (4561, "Theodore Thief", "MISDEMEANEOR", 2, "PENDING", "2013-10-1", "2017-12-20");
+INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status, socialMediaComment)
+VALUES (4971, "Some Guy", "MISDEMEANOR", 2, "REJECTED", "Please send us a warrant.");
 INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status)
-VALUES (13, "Suspect 3", "FELONY", 1, "PENDING");
+VALUES (4317, "Amely Assault", "FELONY", 1, "PENDING");
+INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status)
+VALUES (1182, "Benedict Bomber", "FELONY", 1, "CLOSED");
+INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status, socialMediaComment)
+VALUES (4730, "Tiffany Towed", "MISDEMEANOR", 1, "REJECTED",
+        "Sorry, we were not able to find the user you requested data about. Please provide a new request with more accurate information.");
+INSERT INTO Request (caseID, suspectUserName, caseType, createdByID, status, resultID, socialMediaComment)
+VALUES (4638, "Rick Robber", "FELONY", 1, "ANSWERED", 1, "Attached, please find the requested data.");
 
 INSERT INTO conversation (conversationID, startingTime, participants, resultID)
 VALUES (1, NOW(), "Him Ym, Someone Else", 1);
